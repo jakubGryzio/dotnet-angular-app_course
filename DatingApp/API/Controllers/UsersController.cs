cee3d340f7e3d81dc021213589faad0dcc3e40e3
JakubGryzioHex <86599920+JakubGryzioHex@users.noreply.github.com>
2023-01-26 09:00:51 +0100

    End of section 4

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext context;
 
@@ -16,6 +16,7 @@ namespace API.Controllers
             this.context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -7,5 +7,9 @@
         public string UserName { get; set; }
 
         public string UserEmail { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
